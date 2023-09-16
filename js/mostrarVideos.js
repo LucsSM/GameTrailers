940f58b027e5435d0f9d844a0fddd04f76dece50
Lucas Silva <lucas.silvamarra@gmail.com>
2023-09-16 20:29:45 -0300

    implementação de funções

---
+++ js/mostrarVideos.js
@@ -0,0 +1,30 @@
+import { conectaApi } from "./conectaApi.js";
+
+const lista = document.querySelector("[data-lista]");
+
+function constroiCard(titulo, url, descricao, imagem) {
+    
+    const video = document.createElement("li");
+    video.className = "videos__item";
+    video.innerHTML = `
+        <iframe width="100%" height="72%" src="${url}"
+            title="${titulo}" frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen data-url></iframe>
+        <div class="descricao-video">
+            <img src="${imagem}" alt="logo canal alura" data-imagem>
+            <h3 data-titulo>${titulo}</h3>
+            <p data-descricao>${descricao}</p>
+        </div>
+    `
+
+    return video;
+}
+
+async function mostrarVideos() {
+
+    const listaApi = await conectaApi.listaVideos();
+    listaApi.forEach(elemento => lista.appendChild(constroiCard(elemento.titulo, elemento.url, elemento.descricao, elemento.imagem)))
+}
+
+mostrarVideos();
